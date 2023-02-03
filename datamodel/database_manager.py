468489f7dee3259c3cc07e30bfb0c39cb9478071
Simon Schafferhofer <schaff190163@sr.htlweiz.at>
2023-02-03 12:16:20 +0100

    database manager functions tested and complete

--- datamodel/database_manager.py
+++ datamodel/database_manager.py
@@ -1,6 +1,8 @@
 import datamodel
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import datetime
+
 
 engine = create_engine('sqlite:///C:\\Users\\Simon\\OneDrive\\Dokumente\\4AHIT\\SEW INSY\\WorkTimeTracker\\datamodel\\app_db.sql')
 Session = sessionmaker(bind=engine)
@@ -8,31 +10,31 @@ session = Session()
 
 
 class Database_Manager:
-    def create_user(session, user_name):
+    def create_user(self, session, user_name):
         new_user = datamodel.Users(userName=user_name)
         session.add(new_user)
         session.commit()
         return new_user
 
-    def create_time(session, start_value, stop_value, user_id):
+    def create_time(self, session, start_value, stop_value, user_id):
         new_time = datamodel.Times(startValue=start_value, stopValue=stop_value, userID=user_id)
         session.add(new_time)
         session.commit()
         return new_time
 
-    def read_user(session, user_id):
+    def read_user(self, session, user_id):
         return session.query(datamodel.Users).filter_by(userID=user_id).first()
 
-    def read_times(session, user_id):
+    def read_times(self, session, user_id):
         return session.query(datamodel.Times).filter_by(userID=user_id).all()
 
-    def update_user(session, user_id, username):
+    def update_user(self, session, user_id, user_name):
         user = session.query(datamodel.Users).filter_by(userID=user_id).first()
-        user.userName = username
+        user.userName = user_name
         session.commit()
         return user
 
-    def update_time(session, time_id, start_value, stop_value, user_id):
+    def update_time(self, session, time_id, start_value, stop_value, user_id):
         time = session.query(datamodel.Times).filter_by(timeID=time_id).first()
         time.startValue = start_value
         time.stopValue = stop_value
@@ -40,12 +42,12 @@ class Database_Manager:
         session.commit()
         return time
 
-    def delete_user(session, user_id):
+    def delete_user(self, session, user_id):
         user = session.query(datamodel.Users).filter_by(userID=user_id).first()
         session.delete(user)
         session.commit()
 
-    def delete_time(session, time_id):
+    def delete_time(self, session, time_id):
         time = session.query(datamodel.Times).filter_by(timeID=time_id).first()
         session.delete(time)
         session.commit()
@@ -53,4 +55,3 @@ class Database_Manager:
 
 if __name__ == "__main__":
     dm = Database_Manager()
-    dm.create_user(user_name="simon")
