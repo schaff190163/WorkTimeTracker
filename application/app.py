9e79041e29374cd72f35a28396fbb1683ae9c18d
Simon Schafferhofer <schaff190163@sr.htlweiz.at>
2023-02-09 21:21:47 +0100

    cosmetic changes, selecteduserid still to be imp.

--- application/app.py
+++ application/app.py
@@ -44,66 +44,73 @@ class tkinterApp(tk.Tk):
 class UserSelect(tk.Frame):
 
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.config(bg='red')
+        tk.Frame.__init__(self, parent, width=700, height=450)
 
         mainlabel = ttk.Label(self, text="Select User", font=(
                               'Helvetica bold', 15))
-        mainlabel.grid(row=0, column=0, padx=(0, 0), pady=(10, 0))
+        mainlabel.grid(row=0, column=0, padx=(0, 0), pady=(10, 10))
 
         users = Database_Manager.get_all_users(session)
         users_list = [user.userName for user in users]
         for i, button_name in enumerate(users_list):
-            button = ttk.Button(self, text=button_name, width=20,
+            button = ttk.Button(self, text=button_name, width=50,
                                 command=lambda:
                                 controller.show_frame(Options))
-            button.grid(row=i+1, column=1, padx=(0, 100), pady=(10, 10))
+            button.grid(row=i+1, column=0, padx=(150, 150), pady=(10, 10))
 
 
 class Options(tk.Frame):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.config(bg='green')
+        tk.Frame.__init__(self, parent, width=700, height=450)
 
+        mainlabel = ttk.Label(self, text="Options", font=(
+                              'Helvetica bold', 15))
         actions_button = ttk.Button(self, text="Actions",
                                     command=lambda:
-                                    controller.show_frame(Actions))
+                                    controller.show_frame(Actions),
+                                    width=20)
         stats_button = ttk.Button(self, text="Statistics",
                                   command=lambda:
-                                  controller.show_frame(Statistics))
+                                  controller.show_frame(Statistics),
+                                  width=20)
         back_button = ttk.Button(self, text="Back",
                                  command=lambda:
                                  controller.show_frame(UserSelect))
 
-        actions_button.grid(row=1, column=0, padx=(100, 100), pady=(10, 70))
-        stats_button.grid(row=1, column=2, padx=(100, 100), pady=(10, 70))
-        back_button.grid(row=0, column=0, padx=(100, 100), pady=(5, 100))
+        mainlabel.grid(row=0, column=1, padx=(10, 10), pady=(10, 50))
+        actions_button.grid(row=1, column=0, padx=(75, 75), pady=(25, 85))
+        stats_button.grid(row=1, column=2, padx=(75, 75), pady=(25, 85))
+        back_button.grid(row=0, column=0, padx=(10, 50), pady=(10, 50))
 
 
 class Actions(tk.Frame):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.config(bg='yellow')
+        tk.Frame.__init__(self, parent, width=700, height=450)
 
-        coming_button = ttk.Button(self, text="Coming",
-                                   command=lambda: Database_Manager.create_time
-                                   (session, user_id=1, start_value=now, stop_value=None))
+        mainlabel = ttk.Label(self, text="Are you entering or leaving?", font=(
+                              'Helvetica bold', 15))
+        coming_button = ttk.Button(self, text="Entering",
+                                   command=lambda: [
+                                    Database_Manager.create_time(session, user_id=1, start_value=now, stop_value=None),
+                                    controller.show_frame(Options)
+                                    ], width=20)
         leaving_button = ttk.Button(self, text="Leaving",
-                                    command=lambda: Database_Manager.update_time
-                                    (session, time_id=Database_Manager.get_last_time_id(session), user_id=1, start_value=now, stop_value=now))
+                                    command=lambda: [
+                                    Database_Manager.update_time(session, time_id=Database_Manager.get_last_time_id(session), user_id=1, start_value=now, stop_value=now),
+                                    controller.show_frame(Options)
+                                    ], width=20)
         back_button = ttk.Button(self, text="Back",
                                  command=lambda:
                                  controller.show_frame(Options))
 
-        coming_button.grid(row=1, column=0, padx=(100, 100), pady=(10, 80))
-        leaving_button.grid(row=1, column=2, padx=(100, 100), pady=(10, 80))
-        back_button.grid(row=0, column=0, padx=(100, 100), pady=(10, 80))
-
+        mainlabel.grid(row=0, column=2, padx=(10, 10), pady=(10, 50))
+        coming_button.grid(row=1, column=0, padx=(75, 75), pady=(25, 85))
+        leaving_button.grid(row=1, column=2, padx=(75, 75), pady=(25, 85))
+        back_button.grid(row=0, column=0, padx=(10, 50), pady=(10, 50))
 
 class Statistics(tk.Frame):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.config(bg='blue')
+        tk.Frame.__init__(self, parent, width=700, height=450)
 
         mainlabel = ttk.Label(self, text="Statistics", font=(
                               'Helvetica bold', 15))
@@ -117,14 +124,14 @@ class Statistics(tk.Frame):
                                  command=lambda:
                                  controller.show_frame(Options))
 
-        mainlabel.grid(row=0, column=1, padx=(0, 0), pady=(10, 20))
-        yearlabel.grid(row=1, column=0, padx=(0, 0), pady=(10, 10))
-        monthlabel.grid(row=2, column=0, padx=(0, 0), pady=(10, 10))
-        weeklabel.grid(row=3, column=0, padx=(0, 0), pady=(10, 10))
-        yearval.grid(row=1, column=2, padx=(0, 0), pady=(10, 10))
-        monthval.grid(row=2, column=2, padx=(0, 0), pady=(10, 10))
-        weekval.grid(row=3, column=2, padx=(0, 0), pady=(10, 10))
-        back_button.grid(row=0, column=0, padx=(100, 100), pady=(20, 10))
+        mainlabel.grid(row=0, column=1, padx=(10, 10), pady=(10, 50))
+        yearlabel.grid(row=1, column=0, padx=(30, 30), pady=(10, 10))
+        monthlabel.grid(row=2, column=0, padx=(30, 30), pady=(10, 10))
+        weeklabel.grid(row=3, column=0, padx=(30, 30), pady=(10, 10))
+        yearval.grid(row=1, column=1, padx=(300, 50), pady=(10, 10))
+        monthval.grid(row=2, column=1, padx=(300, 50), pady=(10, 10))
+        weekval.grid(row=3, column=1, padx=(300, 50), pady=(10, 10))
+        back_button.grid(row=0, column=0, padx=(10, 10), pady=(10, 50))
 
 
 app = tkinterApp()
