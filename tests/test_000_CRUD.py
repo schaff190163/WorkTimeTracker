206f226d43c7676106c58a33247ea7956a7d98a3
Simon Schafferhofer <schaff190163@sr.htlweiz.at>
2023-02-10 11:11:50 +0100

    tests would be finished, but imports not working

--- tests/test_000_CRUD.py
+++ tests/test_000_CRUD.py
@@ -1,9 +1,8 @@
-from ..application import datamodel
-from ..application import database_manager
+import application
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from datetime import datetime, timedelta
+from datetime import datetime
 
 engine = create_engine('sqlite:///C:\\Users\\Simon\\OneDrive\\Dokumente\\4AHIT\\SEW INSY\\WorkTimeTracker\\application\\app_db.sql')
 Session = sessionmaker(bind=engine)
@@ -46,6 +45,7 @@ def test_update_time(db_session):
     assert updated_time.startValue == "2020-02-01T00:00:00"
     assert updated_time.stopValue == "2020-02-01T01:00:00"
 
+
 def test_read_times(db_session):
     user = database_manager.Database_Manager.create_user(db_session, "test_user")
     time1 = database_manager.Database_Manager.create_time(db_session, datetime(2022, 1, 1), datetime(2022, 1, 2), user.id)
@@ -55,3 +55,46 @@ def test_read_times(db_session):
     assert time1 in times
     assert time2 in times
 
+
+def test_update_user(db_session):
+    user_name = "John Doe"
+    user = database_manager.Database_Manager.create_user(db_session, user_name)
+    user_id = user.userID
+    updated_user_name = "Jane Doe"
+    updated_user = database_manager.Database_Manager.update_user(db_session, user_id, updated_user_name)
+    assert updated_user.userName == updated_user_name
+    assert updated_user.userID == user_id
+
+
+def test_update_time(db_session):
+    user = database_manager.Database_Manager.create_user(db_session, "test_user")
+    user_id = user.userID
+    start_value = datetime.now()
+    stop_value = datetime.now()
+    time = database_manager.Database_Manager.create_time(db_session, start_value, stop_value, user_id)
+    time_id = time.timeID
+    new_start_value = datetime.now()
+    new_stop_value = datetime.now()
+    updated_time = database_manager.Database_Manager.update_time(db_session, time_id, new_start_value, new_stop_value, user_id)
+    assert updated_time.startValue == new_start_value
+    assert updated_time.stopValue == new_stop_value
+    assert updated_time.userID == user_id
+
+
+def test_delete_user(db_session):
+    user = datamodel.Users(userName="Test User")
+    db_session.add(user)
+    db_session.commit()
+    result = db_session.query(datamodel.Users).filter_by(userName="Test User").first()
+    assert result is not None
+    database_manager.Database_Manager.delete_user(db_session, result.userID)
+    result = db_session.query(datamodel.Users).filter_by(userName="Test User").first()
+    assert result is None
+
+
+def test_delete_time(db_session):
+    user = database_manager.Database_Manager.create_user(db_session, "test_user")
+    time = database_manager.Database_Manager.create_time(db_session, 0, 10, user.userID)
+    database_manager.Database_Manager.delete_time(db_session, time.timeID)
+    times = database_manager.Database_Manager.read_times(db_session, user.userID)
+    assert len(times) == 0
